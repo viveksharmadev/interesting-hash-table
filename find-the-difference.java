71c84a3e6d022fd60902df087335cf45f068bb4b
Learner <vivek.gurukul1982@gmail.com>
2020-10-14 07:31:28 -0700

    Update find-the-difference.java

--- find-the-difference.java
+++ find-the-difference.java
@@ -1,6 +1,25 @@
 // https://leetcode.com/problems/find-the-difference/
 
 class Solution {
+    // tc -> n, sc-> 1
+    public char findTheDifference(String s, String t) {
+        int[] map = new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+            map[s.charAt(i)-'a']++;
+        }
+        
+        for(int i=0; i<t.length(); i++){
+            char c = t.charAt(i);
+            map[c-'a']--;
+            if(map[c-'a'] < 0) return c;
+        }
+        
+        return ' ';
+    }
+}
+
+class Solution {
     // tc -> n, sc-> n
     public char findTheDifference(String s, String t) {
         Map<Character, Integer> freqMap = new HashMap<>();
