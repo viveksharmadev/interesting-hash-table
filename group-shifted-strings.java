abea301bcc234360c036675755b173c2f3b5e592
Learner <vivek.gurukul1982@gmail.com>
2020-10-29 09:47:40 -0700

    Update group-shifted-strings.java

--- group-shifted-strings.java
+++ group-shifted-strings.java
@@ -35,3 +35,39 @@ class Solution {
         return key.toString();
     }
 }
+
+// May be easier to understand
+class Solution {
+    // tc -> m*n, sc-> m*n
+    public List<List<String>> groupStrings(String[] strings) {
+        List<List<String>> result  = new LinkedList<>();
+        
+        if(strings==null || strings.length==0) return result;
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for(String str : strings){
+            String key = getKey(str);
+            map.putIfAbsent(key, new LinkedList<>());
+            map.get(key).add(str);
+        }
+        
+        return new LinkedList<>(map.values());
+    }
+    
+    private String getKey(String str){
+        
+        StringBuilder key = new StringBuilder();
+        
+        for(int i=1; i<str.length(); i++){
+            
+            int diff = str.charAt(i) - str.charAt(i-1);
+            
+            key.append(diff < 0 ? diff+26 : diff);
+            
+            key.append(",");
+        }
+        
+        return key.toString();
+    }
+}
